1d92335e64b4d35f8f35e69564d7416430c6489e
阳君 <937447974@qq.com>
2016-03-28 17:23:57 +0800

    缓存cell提取问题

--- Classes/DataSource/YJTableViewDataSource.m
+++ Classes/DataSource/YJTableViewDataSource.m
@@ -24,20 +24,21 @@
 
 - (UITableViewCell *)dequeueReusableCellWithCellObject:(YJCellObject *)cellObject
 {
-    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellObject.cellName];
+    NSString *identifier = self.cacheCellStrategy == YJTableViewCacheCellDefault ? cellObject.cellName : [NSString stringWithFormat:@"%ld-%ld", cellObject.indexPath.section, cellObject.indexPath.row];
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:identifier];
     if (cell == nil) {
         // 未找到时，重新注入，再寻找
         switch (cellObject.createCell) {
             case YJTableViewCellCreateDefault:
-                [self.tableView registerNib:[UINib nibWithNibName:cellObject.cellName bundle:nil] forCellReuseIdentifier:cellObject.cellName];
-                 cell = [self.tableView dequeueReusableCellWithIdentifier:cellObject.cellName];
+                [self.tableView registerNib:[UINib nibWithNibName:cellObject.cellName bundle:nil] forCellReuseIdentifier:identifier];
+                 cell = [self.tableView dequeueReusableCellWithIdentifier:identifier];
                 break;
                 
             case YJTableViewCellCreateSoryboard:
                 NSLog(@"Soryboard中请使用%@设置cell的Identifier属性", cellObject.cellName);
                 break;
             case YJTableViewCellCreateClass:
-                 cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellObject.cellName];
+                 cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
                 break;
         }
     }
